d96b6ce599eb66091a59de45ca779a886f0570ac
nzkks <kkshanthosh@gmail.com>
2017-01-29 09:16:17 +1300

    Added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var csswring = require('csswring');
-var sass = require('gulp-sass');
+var autoprefixer = require('autoprefixer-core');
 
 gulp.task('styles', function(){
   var processors = [
-    csswring
+    csswring,
+    autoprefixer({browsers: ['last 2 version']})
   ];
 
   return gulp.src('styles.sass')
