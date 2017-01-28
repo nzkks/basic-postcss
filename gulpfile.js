c6ad373ba470f32f77caea3f3178345e0bb4c16f
nzkks <kkshanthosh@gmail.com>
2017-01-29 00:46:16 +1300

    Added gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var csswring = require('csswring');
+var sass = require('gulp-sass');
 
 gulp.task('styles', function(){
   var processors = [
     csswring
   ];
 
-  return gulp.src('styles.css')
+  return gulp.src('styles.sass')
+    .pipe(sass())
     .pipe(postcss(processors))
     .pipe(gulp.dest('./dest'));
 });
 
 gulp.task('watch:styles', function(){
-  gulp.watch('**/*.css', ['styles']);
+  gulp.watch('**/*.sass', ['styles']);
 });
