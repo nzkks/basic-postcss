656a63ca2a854634dc1b9868901935bfdf260eaf
nzkks <kkshanthosh@gmail.com>
2017-01-31 12:00:45 +1300

    Added Pug template

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,18 @@ var postcss = require('gulp-postcss');
 // var csswring = require('csswring'); // Minify CSS using PostCSS
 var autoprefixer = require('autoprefixer-core');
 var lost = require('lost'); // lost grid system for postcss
+var pug = require('gulp-pug');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass', 'pug'], function() {
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: "./dest"
     }
   });
 
-  gulp.watch("./styles.sass", ['sass']);
-  gulp.watch("./index.html").on('change', browserSync.reload);
+  gulp.watch("./src/styles.sass", ['sass']);
+  gulp.watch("./src/index.pug", ['pug']);
 });
 
 gulp.task('sass', function(){
@@ -24,9 +25,16 @@ gulp.task('sass', function(){
     autoprefixer({browsers: ['last 2 version']})
   ];
 
-  return gulp.src('./styles.sass')
+  return gulp.src('./src/styles.sass')
     .pipe(sass())
     .pipe(postcss(processors))
+    .pipe(gulp.dest('./dest'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('pug', function(){
+  return gulp.src('./src/index.pug')
+    .pipe(pug({}))
     .pipe(gulp.dest('./dest'))
     .pipe(browserSync.stream());
 });
